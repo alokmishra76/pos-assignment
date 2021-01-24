b32143b7f3d9bd69696f21ebd0ad25043355b891
alokmishra76 <alokmishra76622@gmail.com>
2021-01-24 18:52:38 +0530

    creating the data file and setting the home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
 import './HomePage.css';
+import RightSide from './RightSide';
+import LeftSide from './LeftSide';
+import {DataProvider} from './homeContext';
+import ProjectData from './productData/pos.product.json';
 
 export default class HomePage extends Component{
 	render(){
 		return(
-			<h1>HomePage</h1>
+			<DataProvider value={{productQtyHandler: this.productQtyHandler, deleteItem: this.deleteItem}}>
+                  <section className="home-screen">
+                     
+                  <LeftSide productQtyHandler={this.productQtyHandler} insertItem={this.state.products} subTotal={this.state.subTotal} cancelSale={this.cancelSale} />
+                  <RightSide projectData={ProjectData} addItem={this.addItem} />
+                  </section>  
+
+                </DataProvider>
 			)
 	}
 }
